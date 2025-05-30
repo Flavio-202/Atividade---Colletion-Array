8de29b0b90397fb82a92eda20ddf031792da8b1a
Rodrigo Aguillar <flavioaguilar2001@gmail.com>
2025-05-30 12:38:10 -0300

    Primeiro commit

---
+++ atividade9.ts
@@ -0,0 +1,4 @@
+//*Escreva um programa para criar uma Collection Array de Objetos do tipo string. O programa deverá solicitar ao usuário, que ele digite via teclado 5 cores e deverá adicioná-las individualmente no Array. Em seguida, faça o que se pede:Mostre na tela todas as cores adicionadas. Mostre na tela todas as cores adicionadas, ordenadas em ordem crescente.
+//*
+
+
