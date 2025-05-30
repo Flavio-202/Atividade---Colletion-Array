398614462cd021997031a414a094e4e7b82c640d
Rodrigo Aguillar <flavioaguilar2001@gmail.com>
2025-05-30 13:06:37 -0300

    Segundo commit

--- atividade9.ts
+++ atividade9.ts
@@ -1,4 +1,18 @@
 //*Escreva um programa para criar uma Collection Array de Objetos do tipo string. O programa deverá solicitar ao usuário, que ele digite via teclado 5 cores e deverá adicioná-las individualmente no Array. Em seguida, faça o que se pede:Mostre na tela todas as cores adicionadas. Mostre na tela todas as cores adicionadas, ordenadas em ordem crescente.
 //*
 
+// escolha 5 cores
+// adicionar no Array as cores
+// mostar na telas todas as cores
+// mostre na tela as cores em ordem crescente.
+
+import leia from "readline-sync"
+
+let listadecores: Array <string> = new Array <string> ()
+
+for (let contador = 0; contador <= 5; contador ++){
+    let cor = leia.question ("Informe uma cor: ");
+    console.log ("A cor informada foi:" + cor);
+}
+
 
